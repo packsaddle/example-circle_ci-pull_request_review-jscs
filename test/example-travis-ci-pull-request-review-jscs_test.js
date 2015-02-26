9f5ed0c46a2eaabca80ce0bfe12b0a88c3aed9fd
sanemat <o.gata.ken@gmail.com>
2015-02-27 05:46:59 +0900

    chore(app): exec yo node-gulp

---
+++ test/example-travis-ci-pull-request-review-jscs_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var exampleTravisCiPullRequestReviewJscs = require('../');
+var assert = require('should');
+
+describe('exampleTravisCiPullRequestReviewJscs', function () {
+
+  it('should be awesome', function () {
+    exampleTravisCiPullRequestReviewJscs().should.equal('awesome');
+  });
+
+});
